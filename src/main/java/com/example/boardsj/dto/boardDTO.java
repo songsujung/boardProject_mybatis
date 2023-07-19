8017cd29d3502fa6c5ab2f62d2fe84882922a4b5
sj <sujung033131@gmail.com>
2023-07-19 22:51:00 +0900

    0719 패키지 생성 + DTO 변수선언

---
+++ src/main/java/com/example/boardsj/dto/boardDTO.java
@@ -0,0 +1,19 @@
+package com.example.boardsj.dto;
+
+import java.util.Date;
+
+public class boardDTO {
+
+    private Long tno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private Date regDate;
+
+    private Date updateDate;
+    
+}
