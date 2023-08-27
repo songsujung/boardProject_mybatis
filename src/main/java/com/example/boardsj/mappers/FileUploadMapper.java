5d30558ba83f978d9ec8e2503fb62ea0f8892558
sj <sujung033131@gmail.com>
2023-08-27 20:14:41 +0900

    0827 파일업로드 Mapper 하는중

---
+++ src/main/java/com/example/boardsj/mappers/FileUploadMapper.java
@@ -0,0 +1,13 @@
+package com.example.boardsj.mappers;
+
+import java.util.List;
+import java.util.Map;
+
+public interface FileUploadMapper {
+
+    // 이미지 및 파일 등록
+    int registerImage(List<Map<String, String>> imageList);
+
+    int deleteImage(Integer bno);
+    
+}
